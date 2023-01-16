0935623ac84e60ec1e93fa5ec9ec823404442518
Gillou68310 <gilles.siberlin@outlook.com>
2023-01-16 10:31:16 +0100

    Decompiled ldist and dist

---
+++ src/code0/41940.c
@@ -0,0 +1,365 @@
+#include "common.h"
+#include "code0/engine.h"
+
+/*.text*/
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80040D40);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80040D94);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80040DFC);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80040E90);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80040EF0);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800413CC);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800418B8);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80041968);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80041D10);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004201C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004204C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042094);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800420C8);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042124);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042140);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042178);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042210);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800422BC);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042380);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042434);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042598);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042860);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042914);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800429B4);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042A6C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042AE4);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042B40);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042B98);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042C18);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80042C98);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800433D4);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004364C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800437F4);
+
+void func_80043870(void)
+{
+}
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80043878);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80043894);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80043938);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800439D0);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80043A74);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80043B10);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80043BAC);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80043CA8);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80043D64);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80043DE4);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80043E7C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80043F3C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80044000);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80044120);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004423C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800442D8);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80044394);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80044854);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800448C0);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004494C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800449DC);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80044A78);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80044B38);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80044F9C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800450E0);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045228);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045400);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800454BC);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045548);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045604);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045690);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045704);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800457BC);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004587C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800458D0);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045988);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045A48);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045A9C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045B04);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045B64);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045C44);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045CB8);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045D34);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045DA8);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80045E40);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800461E8);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80046494);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80046540);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80047710);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80047820);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800494DC);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004A4D4);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004A590);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004AB6C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004B2B0);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004B440);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004BC24);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004BC64);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004BD24);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004BE20);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004BE48);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004BE90);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004BFDC);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004CB3C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004CC90);
+
+/*8004CDE0*/
+s32 dist(SpriteType *s1, SpriteType *s2)
+{
+    return findDistance3D((s1->x - s2->x), (s1->y - s2->y), (s1->z - s2->z) >> 4);
+}
+
+
+/*8004CE24*/
+s32 ldist(SpriteType *s1, SpriteType *s2)
+{
+    return findDistance2D((s1->x - s2->x), (s1->y - s2->y)) + 1;
+}
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004CE58);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004D304);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004D65C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004D7D8);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004D884);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004DC74);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004DE60);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004E5F8);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004E7F0);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004E8BC);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004E9B4);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004EA40);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004EB60);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004EBE4);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004EC38);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004ED40);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004EFB4);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004F044);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004F284);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004F31C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004F4A0);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004F5DC);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004F704);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004F8D0);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004FA74);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004FAD8);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004FC4C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8004FF08);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800502A0);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80050408);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80050480);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800504F4);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8005087C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80050C14);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80050E40);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80051088);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80051330);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800514C8);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80051508);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80051568);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800515A0);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80051684);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800516EC);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80051808);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800519AC);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80052358);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800524BC);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8005259C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80052AB0);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800533C4);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80053650);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80053900);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800539A8);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80055DDC);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80055EC0);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800563D4);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80056600);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80056A54);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80056C00);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_8005731C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800574A4);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80057540);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80057E7C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80058468);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80058538);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80058600);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800586B0);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_800587B8);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80058844);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80058934);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80058A14);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80058B3C);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80058DE0);
+
+INCLUDE_ASM(s32, "src/code0/41940", func_80058E44);
